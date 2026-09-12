a9deace672b9e750b5163e796832fd79a01d1423
ajreider25 <ajreider25@gmail.com>
2026-09-12 03:40:52 -0400

    Created first framework for CLI entry point, removed duplicate main.py, used AI to help build framework for argparsing and binding to commands. created library of basic commands based on user stories

---
+++ commands.py
@@ -0,0 +1,97 @@
+# faculty functions
+def add_faculty():
+    print("implement add faculty function!")
+
+def modify_faculty():
+    print("implement modify faculty function!")
+
+def delete_faculty():
+    print("implement delete faculty function!")
+
+
+# course functions
+def add_course():
+    print("implement add course function!")
+
+def modify_course():
+    print("implement modify course function!")
+
+def delete_course():
+    print("implement delete course function!")
+
+
+# lab functions
+def add_lab():
+    print("implement add course function!")
+
+def modify_lab():
+    print("implement modify lab function!")
+
+def delete_lab():
+    print("implement delete lab function!")
+
+
+# room functions
+def add_room():
+    print("implement add room function!")
+
+def modify_room():
+    print("implement modify room function!")
+
+def delete_room():
+    print("implement delete room function!")
+
+
+# timeslot functions
+def add_timeslot():
+    print("implement add timeslot function!")
+
+def modify_timeslot():
+    print("implement modify timeslot function!")
+
+def delete_timeslot():
+    print("implement delete timeslot function!")
+
+
+# pattern functions
+def add_pattern():
+    print("implement add pattern function!")
+
+def modify_pattern():
+    print("implement modify pattern function!")
+
+def delete_pattern():
+    print("implement delete pattern function!")
+
+
+# meeting functions
+def add_meeting():
+    print("implement add meeting function!")
+
+def modify_meeting():
+    print("implement modify meeting function!")
+
+def delete_meeting():
+    print("implement delete meeting function!")
+
+
+# config functions
+def print_config():
+    print("implement print config function!")
+
+def save_config():
+    print("implement save config function!")
+
+def load_config():
+    print("implement load config function!")
+
+
+# schedule functions
+def schedule_summary():
+    print("implement schedule summary function!")
+def modify_schedule():
+    print("implement modify schedule function!")
+def replace_schedule():
+    print("implement replace schedule function!")
+def export_schedule():
+    print("implement export schedule function!")
